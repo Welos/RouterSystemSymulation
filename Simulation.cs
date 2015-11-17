a9801c51680849444ab6c589da9ebe39ad9b5b6f
bbielecki <bartlomiejbielecki1@gmail.com>
2015-11-17 21:38:42 +0100

    Update Simulation.cs

--- Simulation.cs
+++ Simulation.cs
@@ -10,31 +10,38 @@ namespace Router
 {
     class Simulation
     {
-        double[] NumberOfReceivedPackages;
-        double[] NumberOfServedPackages;
-        double[] ProbabilityOfFailure;
-        string[] TableOfPriorities;
+        double[] NumberOfReceivedPackages;//
+        double[] NumberOfServedPackages;//
+        double[] ProbabilityOfFailure;//
+        string[] TableOfPriorities;//
         int NumberOfDistributions;
-        int[] AverageServingTime;
+        double[] AverageServingTime;//
+        double[] TotalServingTime;//
+        double[] TotalOccupancyOfQueue;//
+        double[] AverageOccupancyOfqueue;//
         int NumberOfQueues;
         int NumberOfStreams;
         double TimeOfSimulation;
         double CurrentTime;
         double AuxilaryTime;
-        ProbabilityDistributions[] distribution;
-        FIFOQueue[] queue;
-        Heap  eventQueue;
-        Stream[] stream;
+        ProbabilityDistributions[] distribution;//
+        FIFOQueue[] queue;//
+        Heap  eventQueue;//
+        Stream[] stream;//
         bool occupancyOfRouter;
         string inputDirectory;
         string outputDirectory;
         string systemName;
         int flowability;
         Package ProcessedPackage;
+        double TotalOccupancyOfRouter;
+        double AverageOccupancyOfRouter;
 
         public Simulation ()
         {
+            TotalOccupancyOfRouter = 0;
             Package ProcessedPackage = new Package();
+            NumberOfStreams = 0;
             GetData();
             TimeOfSimulation = CurrentTime =AuxilaryTime= 0;
             occupancyOfRouter = false;
@@ -42,7 +49,27 @@ namespace Router
             NumberOfReceivedPackages = new double[NumberOfStreams + 1];
             NumberOfServedPackages = new double[NumberOfStreams + 1];
             ProbabilityOfFailure = new double[NumberOfStreams + 1];
-            AverageServingTime = new int[NumberOfStreams + 1];
+            AverageServingTime = new double[NumberOfStreams + 1];
+            TotalServingTime = new double[NumberOfStreams + 1];
+            AverageOccupancyOfqueue = new double[NumberOfStreams];
+            TotalOccupancyOfQueue = new double[NumberOfStreams];
+            TotalOccupancyOfRouter = 0;
+            AverageOccupancyOfRouter = 0;
+            for (int i=0;i<=NumberOfStreams;i++)
+            {
+                TotalServingTime[i] = 0;
+                AverageServingTime[i] = 0;
+                NumberOfReceivedPackages[i] = 0;
+                NumberOfServedPackages[i] = 0;
+                ProbabilityOfFailure[i] = 0;
+            }
+            for (int i = 0; i < NumberOfStreams; i++)
+            {
+                AverageOccupancyOfqueue[i] = 0;
+                TotalOccupancyOfQueue[i] = 0;
+            }
+
+
 
         }
         public void fSimulation()
@@ -65,7 +92,15 @@ namespace Router
                 }
                 for (int i=0;i<NumberOfStreams;i++)
                 {
-                    temporarytime = distribution[stream[i].GetNumberOfWaitingDistribution()].SetTime(rnd.NextDouble());
+                    int a=0;
+                    string CurrentLambda=stream[i].GetNameOfWaitingDistribution();
+                    for (int j=0;j<NumberOfStreams;j++)
+                    {
+                        if(CurrentLambda==distribution[j].GetName())
+                            a=j;
+
+                    }
+                    temporarytime = distribution[a].SetTime(rnd.NextDouble());
                     eventQueue.Insert(Event.Coming(i, temporarytime)); //Obczaić o co chodzi z kluczem
                 }
 
@@ -75,7 +110,12 @@ namespace Router
                 if (AuxilaryTime == 0)
                     AuxilaryTime = CurrentTime;
                 else AuxilaryTime = CurrentTime - previoustime;
-
+                for (int i=0;i<NumberOfStreams;i++)
+                {
+                    TotalOccupancyOfQueue[i] += queue[i].GetOccupancy() * AuxilaryTime;
+                }
+                if (occupancyOfRouter == true)
+                    TotalOccupancyOfRouter += AuxilaryTime;
                 Event TempEvent=eventQueue.Delete();
 
 
@@ -87,11 +127,14 @@ namespace Router
                               Package TemporaryPackage = new Package();
                               previoustime = CurrentTime;
                               CurrentTime = TempEvent.getKey();
+                              NumberOfReceivedPackages[TempEvent.getStreamNumber()]++;
+                              NumberOfReceivedPackages[NumberOfStreams ]++;
                          // CurrentTime+=distribution[stream[TempEvent.getStreamNumber()].GetNumberOflengthDistribution()].SetTime(rnd.NextDouble());
                           //ustalenie nr bufora i strumienia:
                               if (occupancyOfRouter==false)
                               {
                                   occupancyOfRouter = true;
+                                  a
                                   double processing = (distribution[stream[TempEvent.getStreamNumber()].GetNumberOflengthDistribution()].SetTime(rnd.NextDouble()))/flowability;
                                   eventQueue.Insert(Event.GoingOutOfRouter(TempEvent.getStreamNumber(), CurrentTime+processing));
                                   ProcessedPackage = TemporaryPackage;
@@ -101,13 +144,19 @@ namespace Router
                               {
                                   queue[stream[TempEvent.getStreamNumber()].bufor].SetPackage(TemporaryPackage);
                               }
-                                  double interval = distribution[stream[TempEvent.getStreamNumber()].GetNumberOfWaitingDistribution()].SetTime(rnd.NextDouble());
-                                  eventQueue.Insert(Event.Coming(TempEvent.getStreamNumber(), CurrentTime + interval));
+                              
+                              a
+                              double interval = distribution[stream[TempEvent.getStreamNumber()].GetNumberOfWaitingDistribution()].SetTime(rnd.NextDouble());
+                              eventQueue.Insert(Event.Coming(TempEvent.getStreamNumber(), CurrentTime + interval));
+                                    
                           break;
                           }
                       case KindOfEvent.GoingOutOfRouter:
                           {
-                              //zbieramy staty
+                              TotalServingTime[TempEvent.getStreamNumber()] += (TempEvent.getKey() - ProcessedPackage.GetComingTime());
+                              TotalServingTime[NumberOfStreams] += (TempEvent.getKey() - ProcessedPackage.GetComingTime());
+                              NumberOfServedPackages[TempEvent.getStreamNumber()]++;
+                              NumberOfServedPackages[NumberOfStreams ]++;
                               occupancyOfRouter = false;
                               previoustime = CurrentTime;
                               CurrentTime = TempEvent.getKey();
@@ -115,17 +164,15 @@ namespace Router
                                   {
                                       if (queue[i].occupancy != 0)
                                       {
-
+                                          occupancyOfRouter = true;
                                           ProcessedPackage = queue[i].MoveToStream();
+                                          a
                                           double processing = (distribution[stream[TempEvent.getStreamNumber()].GetNumberOflengthDistribution()].SetTime(rnd.NextDouble())) / flowability;
                                           eventQueue.Insert(Event.GoingOutOfRouter(TempEvent.getStreamNumber(), CurrentTime + processing));
 
                                           break;
                                       }
                                   }
-                                  //no i tu się jebie, chyba lecimy po buforach i z najwyższego priorytetu bierzemy package i robimy nowe 
-                                  //wydarzenie GoingOutOfRouter
-                                  //zmniejszamy zajętość bufora z którego wzięliśmy
                                   break;
                           }
 
@@ -137,10 +184,7 @@ namespace Router
             Console.WriteLine("Zakonczono symulacje");
 
         }
-        public void SumUp()
-        {
-            //zbieramy zajętość, czas obsługi, pstwo odrzucenia
-        }
+
         public void GetData()
         {
 
@@ -322,21 +366,48 @@ namespace Router
                     Console.WriteLine(" ");
                     if (outputDirectory[0] == '"') outputDirectory = outputDirectory.Substring(1, outputDirectory.Length - 2);
                 }
-                catch (Exception e)
+                catch (Exception def)
                 {
                     Console.WriteLine("Zla sciezka. Sprobuj jeszcze raz.");
-                    Console.WriteLine(e.Message);
+                    Console.WriteLine(def.Message);
                     ok = false;
                 }
 
             }
         }
 
+        public void SumUp()
+        {
+            for (int i = 0; i < NumberOfStreams; i++)
+            {
+                AverageOccupancyOfqueue[i] = TotalOccupancyOfQueue[i] / (TimeOfSimulation * queue[i].GetSize());
+                AverageServingTime[i] = TotalServingTime[i] / NumberOfServedPackages[i];
+                ProbabilityOfFailure[i] = (NumberOfReceivedPackages[i] - NumberOfServedPackages[i]) / NumberOfReceivedPackages[i];
+            }
+            AverageServingTime[NumberOfStreams ] = TotalServingTime[NumberOfStreams ] / NumberOfServedPackages[NumberOfStreams ];
+            ProbabilityOfFailure[NumberOfStreams] = (NumberOfReceivedPackages[NumberOfStreams ] - NumberOfServedPackages[NumberOfStreams ]) / NumberOfReceivedPackages[NumberOfStreams ];
+            AverageOccupancyOfRouter = TotalOccupancyOfRouter / TimeOfSimulation;
+            //zbieramy zajętość, czas obsługi, pstwo odrzucenia
 
+        }
         public void WriteResults()
         {
-            //mniejsze pierdolenie
-        }
-        }
+            StreamWriter routerSimulationResults = new StreamWriter(outputDirectory);
 
+            routerSimulationResults.WriteLine("System = " + systemName);
+            for (int i=0; i<NumberOfStreams;i++)
+            {
+                routerSimulationResults.WriteLine("Srednia zajetosc bufora {0} : " + AverageOccupancyOfqueue[i], TableOfPriorities[i]);
+                routerSimulationResults.WriteLine("Sredni czas obslugi pakietu o priorytecie {0} : " + AverageServingTime[i], TableOfPriorities[i]);
+                routerSimulationResults.WriteLine("Prawdopodobienstwo odrzucenia ze strumienia {0} : " + ProbabilityOfFailure[i], i);
+            }
+
+            routerSimulationResults.WriteLine();
+            routerSimulationResults.WriteLine("Sredni czas obsugi pakietu : " + AverageServingTime[NumberOfStreams]);
+            routerSimulationResults.WriteLine("Prawdopodobienstwo odrzucenia z Routera : " + ProbabilityOfFailure[NumberOfStreams ]);
+            routerSimulationResults.WriteLine("Srednia zajetosc Routera : " + AverageOccupancyOfRouter);
+
+            routerSimulationResults.Close();
+        }
     }
+}
