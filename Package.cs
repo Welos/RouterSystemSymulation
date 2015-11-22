452249f1629515710d59b17e591059921c60f133
bbielecki <bartlomiejbielecki1@gmail.com>
2015-11-22 13:02:17 +0100

    Update Package.cs

--- Package.cs
+++ Package.cs
@@ -6,18 +6,22 @@ using System.Threading.Tasks;
 
 namespace Router
 {
-    class Package
-    {
-        private double size;
+    class Package  // klasa pakietu obslugiwanego przez router / 
+    {              
+        private static int numberOfPackages=0;
+        private double size;      // wielkosc paczki w bajtach
         private double comingTime;// czas w którym połącznie pojawiło się w strumieniu 
-        public Package()
+        public Package()          // konstruktor domyslny, nie uzywany w symulacji
         {
+            numberOfPackages++;
         }
         public Package( double m_size, double m_comingTime)
         {
+            numberOfPackages++;
             size = m_size;
             comingTime = m_comingTime;
         }
+        public static int GetNumberOfPackages() { return numberOfPackages; }
         public double GetSize() { return size; }
         public double GetComingTime() { return comingTime; }
     }
